8d2af94b5446552384c9619abe5b9602ed59a7aa
LinaMassoud <Sp.Linamassoud@reachdigitalhub.com>
2025-07-16 13:08:19 +0300

    change bubleid and applicationid

--- android/app/src/main/kotlin/com/emdadHR/fawran/MainActivity.kt
+++ android/app/src/main/kotlin/com/emdadHR/fawran/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.fawran
+package com.emdadHR.fawran
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/generated/app_localizations_ar.dart
+++ lib/generated/app_localizations_ar.dart
@@ -109,4 +109,308 @@ class AppLocalizationsAr extends AppLocalizations {
 
   @override
   String get welcome => 'أهلا';
+
+  @override
+  String get designYourCard => 'صمم بطاقتك';
+
+  @override
+  String get selectService => 'اختر الخدمة';
+
+  @override
+  String get hourlyServices => 'خدمات كل ساعة';
+
+  @override
+  String get addNewAddress => 'إضافة عنوان جديد';
+
+  @override
+  String get selectAddressTitle => 'حدد العنوان';
+
+  @override
+  String get next => 'التالي';
+
+  @override
+  String get nationality => 'جنسية';
+
+  @override
+  String get professionals => 'كم عدد المحترفين الذين تحتاجهم';
+
+  @override
+  String get contractDuration => 'مدة العقد';
+
+  @override
+  String get time => 'وقت';
+
+  @override
+  String get durationOfVisit => 'مدة الزيارة';
+
+  @override
+  String get visitsWeeksNumber => 'عدد الزيارات في الاسبوع';
+
+  @override
+  String get date => 'حدد التاريخ';
+
+  @override
+  String get select => 'يختار';
+
+  @override
+  String get tapToSelect => 'انقر للتحديد';
+
+  @override
+  String get dialogForPrevious =>
+      'الرجاء تحديد مدة العقد وعدد الزيارات في الأسبوع أولاً';
+
+  @override
+  String get selectNationality => 'اختر الجنسية';
+
+  @override
+  String get selectContractDuration => 'حدد مدة العقد';
+
+  @override
+  String get selectTimeSlot => 'حدد فتحة زمنية';
+
+  @override
+  String get selectVisitDuration => 'حدد فتحة زمنية';
+
+  @override
+  String get selectVisitsPerWeek => 'اختر عدد الزيارات في الأسبوع';
+
+  @override
+  String get noOfEmployee => 'عدد الموظفين';
+
+  @override
+  String get duration => 'مدة';
+
+  @override
+  String get weeklyVisits => 'الزيارات الأسبوعية';
+
+  @override
+  String get totalPrice => 'السعر الإجمالي';
+
+  @override
+  String get packageDetails => 'تفاصيل الحزمة';
+
+  @override
+  String get get => 'يحصل';
+
+  @override
+  String get off => 'عن';
+
+  @override
+  String get hours => 'ساعات';
+
+  @override
+  String get weeks => 'أسابيع';
+
+  @override
+  String get add => 'يضيف';
+
+  @override
+  String get insertAddress => 'أدخل العنوان';
+
+  @override
+  String get district => 'يصرف';
+
+  @override
+  String get map => 'رسم خريطة';
+
+  @override
+  String get details => 'تفاصيل';
+
+  @override
+  String get addressTitle => 'عنوان العنوان';
+
+  @override
+  String get houseType => 'نوع المنزل';
+
+  @override
+  String get buildingNum => 'رقم المبنى';
+
+  @override
+  String get streetName => 'اسم الشارع';
+
+  @override
+  String get villa => 'فيلا';
+
+  @override
+  String get appartment => 'شقة';
+
+  @override
+  String get appartmentNumber => 'رقم الشقة';
+
+  @override
+  String get flooeNumber => 'رقم الطابق';
+
+  @override
+  String get fullAddress => 'العنوان الكامل';
+
+  @override
+  String get houseNum => 'رقم المنزل';
+
+  @override
+  String get floor => 'أرضية';
+
+  @override
+  String get notes => 'ملحوظات';
+
+  @override
+  String get selectCity => 'الرجاء تحديد مدينتك';
+
+  @override
+  String get selectDistrict => 'الرجاء تحديد منطقتك';
+
+  @override
+  String get selectMap => 'اختر على الخريطة';
+
+  @override
+  String get selectAddress => 'يرجى إعطاء العنوان اسمًا';
+
+  @override
+  String get selectHouseType => 'اختر نوع المنزل';
+
+  @override
+  String get selectFloor => 'اختر الكلمة';
+
+  @override
+  String get selectNote => 'رقم الوحدة، رمز الدخول، إلخ';
+
+  @override
+  String get selectLocation => 'حدد الموقع';
+
+  @override
+  String get edit => 'يحرر';
+
+  @override
+  String get mapSelected => 'تم تحديد الخريطة';
+
+  @override
+  String get save => 'يحفظ';
+
+  @override
+  String get step => 'خطوة';
+
+  @override
+  String get ofStep => 'ل';
+
+  @override
+  String get selectDate => 'حدد التاريخ';
+
+  @override
+  String get total => 'المجموع';
+
+  @override
+  String get start => 'يبدأ';
+
+  @override
+  String get congratulations => 'تهانينا!';
+
+  @override
+  String get saved => 'المحفوظة حتى الآن!';
+
+  @override
+  String get viewOrder => 'عرض الطلب';
+
+  @override
+  String get orderSummary => 'ملخص الطلب';
+
+  @override
+  String get serviceDetails => 'تفاصيل الخدمة';
+
+  @override
+  String get startDate => 'تاريخ البدء';
+
+  @override
+  String get workers => 'العمال';
+
+  @override
+  String get finalPrice => 'السعر النهائي';
+
+  @override
+  String get billingPayment => 'الفواتير والدفع';
+
+  @override
+  String get coupons => 'القسائم والعروض';
+
+  @override
+  String get paymentSummary => 'ملخص الدفع';
+
+  @override
+  String get itemTotal => 'إجمالي السلعة';
+
+  @override
+  String get packDiscount => 'خصم الحزمة';
+
+  @override
+  String get month => 'شهر';
+
+  @override
+  String get visitWeekly => 'زيارة أسبوعية';
+
+  @override
+  String get savedSummary => 'ياااي! لقد أنقذت';
+
+  @override
+  String get onFinalBill => 'على الفاتورة النهائية';
+
+  @override
+  String get agreement => 'أنا أوافق على ';
+
+  @override
+  String get termsAndCond => 'الشروط والأحكام';
+
+  @override
+  String get close => 'يغلق';
+
+  @override
+  String get proceedToPay => 'المضي قدما في الدفع';
+
+  @override
+  String get done => 'منتهي';
+
+  @override
+  String get totalIncVat => 'الإجمالي (شامل ضريبة القيمة المضافة)';
+
+  @override
+  String get eastAsia => 'شرق آسيا';
+
+  @override
+  String get africa => 'الأفريقي';
+
+  @override
+  String get months => 'شهور';
+
+  @override
+  String get serviceContractId => 'معرف عقد الخدمة';
+
+  @override
+  String get contractId => 'معرف العقد';
+
+  @override
+  String get customer => 'عميل';
+
+  @override
+  String get service => 'خدمة';
+
+  @override
+  String get vat => 'ضريبة';
+
+  @override
+  String get notConfirmed => 'لم يتم تأكيده';
+
+  @override
+  String get confirmed => 'مؤكد';
+
+  @override
+  String get cancelled => 'تم الإلغاء';
+
+  @override
+  String get paid => 'مدفوع';
+
+  @override
+  String get hourlyService => 'خدمة كل ساعة';
+
+  @override
+  String get payNow => 'ادفع الآن';
+
+  @override
+  String get cancel => 'يلغي';
 }
